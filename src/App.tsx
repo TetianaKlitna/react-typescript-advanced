0c994c2b5be5327c37c9349f1a865adfdaf8daa7
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-07-30 07:49:05 -0700

    'Form'

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import Input from './components/Input';
 import Button from './components/Button';
 import Container from './components/Container';
+import Form from './components/Form';
 import { useRef } from 'react';
 
 function App() {
   const ref = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input label="Your name:" id="name" type="text" ref={ref}/>
-      <Input label="Your age:" id="age" type="number" />
-      <p>
-        <Button>A Button</Button>
-      </p>
+      <Form>
+        <Input label="Name:" id="name" type="text" ref={ref} />
+        <Input label="Age:" id="age" type="number" />
+        <p>
+          <Button>Safe</Button>
+        </p>
+      </Form>
       <p>
         <Button href="https://google.com">A Link</Button>
       </p>
-      <Container as={Button} onClick={() => alert('Hello')}>Click me!</Container>
+      <Container as={Button} onClick={() => alert('Hello')}>
+        Click me!
+      </Container>
     </main>
   );
 }
